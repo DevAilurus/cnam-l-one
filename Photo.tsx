f67f3a0949e85e3c5be93f5bd5118c84c8f5ef8e
DevAilurus <devailurus@gmail.com>
2024-10-21 22:51:32 +0200

    resolve

---
+++ Photo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Frame 2" tilewidth="32" tileheight="32" tilecount="56" columns="7">
+ <image source="../../../Downloads/Frame 2.png" width="241" height="266"/>
+</tileset>
--- office-chunk-1-399eb3e3.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="office-chunk-1-399eb3e3" tilewidth="32" tileheight="32" tilecount="225" columns="15">
- <image source="office-chunk-1-399eb3e3.png" width="480" height="480"/>
-</tileset>
