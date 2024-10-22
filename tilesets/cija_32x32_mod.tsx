30a9d0e6aead9241b68c35e1f49920acd5cbace7
DevAilurus <devailurus@gmail.com>
2024-10-22 08:54:10 +0200

    test new

--- tilesets/cija_32x32_mod.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="cija_32x32_mod" tilewidth="32" tileheight="32" tilecount="2394" columns="18">
- <image source="../images/img/cija_32x32_mod.png" width="576" height="4256"/>
-</tileset>
--- tilesets/taillecrayon.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="taillecrayon" tilewidth="32" tileheight="32" tilecount="56" columns="7">
- <image source="../images/img/taillecrayon.png" width="241" height="266"/>
-</tileset>
